bc43e6faef52dd95e535149ecc68e49d45a31801
Bayron Muñoz <bayronjosue17@gmail.com>
2021-10-18 18:58:20 -0600

    Agregar archivos de proyecto.

---
+++ RegistroLogin/RegistroLogin/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RegistroLogin
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new VIewsAcceso.InicioPage());
+            //MainPage = new InicioPage();
+
+            //Background color
+            MainPage.SetValue(NavigationPage.BarBackgroundColorProperty, Color.Aquamarine);
+
+            
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ RegistroLogin/RegistroLogin/VIewsAcceso/NewPassPage1.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RegistroLogin.VIewsAcceso
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NewPassPage1 : ContentPage
+    {
+        public NewPassPage1()
+        {
+            InitializeComponent();
+        }
+    }
+}
